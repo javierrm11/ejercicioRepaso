a0e828bf9060a7956f977e32b75e0ad8b6300739
jose.gomez <javierrumo2@gmail.com>
2024-02-06 17:02:58 +0100

    subida

---
+++ src/Mascota.java
@@ -0,0 +1,13 @@
+public class Mascota {
+    String nombre;
+    int edad;
+    String tipoAnimal;
+
+    public Mascota(String nombre, int edad, String tipoAnimal){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.tipoAnimal = tipoAnimal
+    }
+
+
+}
